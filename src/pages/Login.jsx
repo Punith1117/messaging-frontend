e2d3c52f26897c539caec82efbf3fc941e31765e
Punith1117 <punith11172006@gmail.com>
2026-01-31 18:39:21 +0530

    feat(login): handle username & password input with signup link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,56 @@
-const Login = () => {
-    return <div>Login</div>
-}
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { loginUser } from "../api/authQueries";
+import { setToken } from "../utils";
 
-export default Login
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null)
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { token } = await loginUser(username, password);
+      setToken(token);
+      navigate("/profile", {replace: true});
+    } catch (err) {
+      setError(err.message)
+    }
+  };
+
+  return (
+    <div>
+        <div>
+            <p>Hello Again!</p>
+            <p>Welcome Back</p>
+        </div>
+        <form onSubmit={handleSubmit}>
+        <input
+            placeholder="Username"
+            value={username}
+            onChange={(e) => {
+                setUsername(e.target.value)
+                if (error) setError(null)
+            }}
+        />
+        <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => {
+                setPassword(e.target.value)
+                if (error) setError(null)
+            }}
+        />
+        {error && <p>{error}</p>}
+        <button type="submit">Login</button>
+        </form>
+        <Link to={'/signup'}>Signup instead</Link>
+    </div>
+  );
+};
+
+export default Login;
