ca96ac320d37b44ed1f99ab9ee1e4afc12de21ca
Punith1117 <punith11172006@gmail.com>
2026-01-31 20:45:08 +0530

    feat(authQueries): add method to create new user

--- src/api/authQueries.js
+++ src/api/authQueries.js
@@ -15,6 +15,22 @@ const loginUser = async (username, password) => {
     return res
 }
 
+const signupUser = async (username, password, casualName) => {
+    let res = await fetch('http://localhost:3000/auth/signup', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({username, password, casualName})
+    })
+
+    res = await res.json()
+    if (res.error) {
+        throw new Error(res.error.message)
+    }
+}
+
 export {
-    loginUser
+    loginUser,
+    signupUser
 }
