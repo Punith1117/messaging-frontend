1878929b08e9652aded5d2d35bd22a9ca67def70
Punith1117 <punith11172006@gmail.com>
2026-02-01 23:32:53 +0530

    feat(components): implement component to display user details
    - these details are of other users

---
+++ src/components/UserDetails.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react"
+import { useNavigate, useParams } from "react-router-dom"
+import { getUserDetails } from "../api/userQueries"
+
+const UserDetails = () => {
+    const { userId } = useParams()
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const navigate = useNavigate()
+
+    const fetchUser = async () => {
+        try {
+            setLoading(true)
+            const data = await getUserDetails(userId)
+            if (data === 401) {
+                navigate('/login', {replace: true})
+                return                
+            } else if (data === 404) {
+                navigate('/') // page not found
+                return
+            }
+            setUser(data)
+        } catch (err) {
+            setError("Failed to fetch user details")
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, [userId])
+
+    if (loading) return <p>Loading user details...</p>
+    if (error) return <p>{error}</p>
+    if (!user) return <p>User not found</p>
+
+    return (
+        <div>
+            <p>{user.username}</p>
+            {user.casualName && (
+                <p>{user.casualName}</p>
+            )}
+            {user.mood && (
+                <p>{user.mood}</p>
+            )}
+        </div>
+    )
+}
+
+export default UserDetails
