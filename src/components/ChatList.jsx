34bf1a89729a6b5c9adbb2201cc49639d1b2b5aa
Punith1117 <punith11172006@gmail.com>
2026-02-01 19:28:19 +0530

    feat(ChatList): add component to fetch and display chats of user

---
+++ src/components/ChatList.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+import getAllChats from "../api/chatQueries"
+
+const ChatList = () => {
+    const [chats, setChats] = useState([])
+    const [loading, setLoading] = useState(true)
+    const navigate = useNavigate()
+
+    const fetchChats = async () => {
+        let res = await getAllChats();
+        setLoading(false)
+        if (res === 401) {
+            navigate('/login', {replace: true})
+            return
+        }
+        setChats(res)
+    }
+    
+    useEffect(() => {
+        fetchChats()
+    }, [navigate])
+
+    if (loading) return <div>loading...</div>
+    else if (chats.length === 0) {
+        return (
+            <div>
+                <p>No chat found</p>
+                <p>Search for new users and start a chat</p>
+            </div>
+        )
+    } else return (
+        <div>
+            {
+                chats.map(item => ( 
+                    <ChatListItem
+                        key={item.id} 
+                        id={item.user.id} 
+                        username={item.user.username}
+                        casualName={item.user.casualName}
+                        status={item.status} 
+                />))
+            }
+        </div>
+    )
+}
+
+const ChatListItem = ({id, username, casualName, status}) => {
+    return (
+        <Link to={`/chat/${id}`}>
+            <p>{username}</p>
+            {casualName && <p>{casualName}</p>}
+            {(status !== 'accepted') && <p>{status}</p>}
+        </Link>
+    )
+}
+
+export default ChatList
