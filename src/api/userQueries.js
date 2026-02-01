a7cb4afca241cc8dce2d190c142e193e092e1117
Punith1117 <punith11172006@gmail.com>
2026-02-01 23:00:16 +0530

    feat(api): add queries to get/patch user details - add a query to get available moods

---
+++ src/api/userQueries.js
@@ -0,0 +1,53 @@
+import { getToken } from "../utils"
+
+const url = 'http://localhost:3000'
+const saveMyDetails = async (casualName, mood) => {
+    try {
+        let res = await fetch(`${url}/profile`, {
+            method: 'PATCH',
+            headers: {
+                "authorization": `Bearer ${getToken()}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                casualName,
+                mood
+            })
+        })
+        if (res.status === 401) return 401
+        return true
+    } catch (e) {
+        console.log(e)
+        return false
+    }
+}
+
+const getMyDetails = async () => {
+    let res = await fetch(`${url}/profile`, {
+        method: 'GET',
+        headers: {
+            "authorization": `Bearer ${getToken()}`
+        }
+    })
+
+    if (res.status === 401) return res.status
+
+    res = await res.json()
+
+    return res.profile
+}
+
+const getAvailableMoods = async () => {
+    let res = await fetch (`${url}/meta/moods`, {
+        method: 'GET'
+    })
+
+    res = await res.json()
+    return res.moods
+}
+
+export {
+    saveMyDetails,
+    getMyDetails,
+    getAvailableMoods
+}
