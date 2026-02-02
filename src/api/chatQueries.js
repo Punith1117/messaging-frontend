68b8f91f56d59bb4af4e4585fb7a79c7c11afe9b
Punith1117 <punith11172006@gmail.com>
2026-02-02 17:10:06 +0530

    feat(api): add query to send message to different users

--- src/api/chatQueries.js
+++ src/api/chatQueries.js
@@ -16,6 +16,32 @@ const getAllChats = async () => {
     return res.chats
 }
 
+const sendMessage = async (toId, content) => {
+    toId = parseInt(toId)
+    let res
+    try {
+        res = await fetch(`${url}/message`, {
+            method: 'POST',
+            headers: {
+                "authorization": `Bearer ${getToken()}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                toId,
+                content
+            })
+        })
+
+        if(!res.ok) {
+            console.log("Error: " + res.status)
+            return
+        }
+    } catch (e) {
+        console.error(e.message)
+    }
+}
+
 export {
-    getAllChats
+    getAllChats,
+    sendMessage
 }
