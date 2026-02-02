8f64d6e907c69214eef30d96c03f7810d6bc0940
Punith1117 <punith11172006@gmail.com>
2026-02-02 19:01:46 +0530

    feat(pages): add UserDetails & MessageBox components

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,5 +1,13 @@
+import MessageBox from "../components/MessageBox"
+import UserDetails from "../components/UserDetails"
+
 const Chat = () => {
-    return <div>Chat</div>
+    return (
+        <div>
+            <UserDetails />
+            <MessageBox />
+        </div>
+    )
 }
 
 export default Chat
