3c55361f7db071651426964d6f7864780663067e
Punith1117 <punith11172006@gmail.com>
2026-02-03 16:35:43 +0530

    style(UserDetails): add styles with required images for moods

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import { getUserDetails } from "../api/userQueries"
+import sadIcon from "../assets/sad.png"
+import angryIcon from "../assets/angry.png"
+import happyIcon from "../assets/happy.png"
+import neutralIcon from "../assets/neutral.png"
+import styled from "styled-components"
 
 const UserDetails = () => {
     const { userId } = useParams()
@@ -32,20 +37,74 @@ const UserDetails = () => {
         fetchUser()
     }, [userId])
 
-    if (loading) return <p>Loading user details...</p>
-    if (!user) return <p>User not found</p>
+    if (loading) return <Wrapper>Loading user details...</Wrapper>
+    if (!user) return <Wrapper>User not found</Wrapper>
 
     return (
-        <div>
-            <p>{user.username}</p>
-            {user.casualName && (
-                <p>{user.casualName}</p>
-            )}
+        <Wrapper>
+            <LeftColumn>
+                <p className="username">{user.username}</p>
+                {user.casualName && (
+                    <p className="casual-name">{user.casualName}</p>
+                )}
+            </LeftColumn>
+
             {user.mood && (
-                <p>{user.mood}</p>
+                <Mood $mood={user.mood} >{user.mood}</Mood>
             )}
-        </div>
+        </Wrapper>
     )
 }
 
 export default UserDetails
+
+const moodBgMap = {
+    sad: sadIcon,
+    angry: angryIcon,
+    happy: happyIcon,
+    neutral: neutralIcon,
+}
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 10%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 50px;
+`;
+
+const LeftColumn = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    p {
+        margin: 0;
+    }
+
+    .username {
+        font-size: 20px;
+        font-weight: 600;
+    }
+
+    .casual-name {
+        font-weight: 600;
+        color: #8f8f8f;
+    }
+`;
+
+const Mood = styled.div`
+    height: 60px;
+    width: 60px;
+    display: flex;
+    align-items: center;
+    color: transparent;
+    background-image: ${({ $mood }) =>
+        $mood ? `url(${$mood && moodBgMap[$mood]})` : "none"};
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 50px 50px;
+
+    background-color: #f7e3ff;
+`;
