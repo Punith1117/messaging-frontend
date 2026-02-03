5fa0cd0ec6d319ce40a1055724d6db926bae0704
Punith1117 <punith11172006@gmail.com>
2026-02-03 15:13:38 +0530

    style(Profile): add styles to Profile component and image for edit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { getAvailableMoods, getMyDetails, saveMyDetails } from "../api/userQueries"
 import { useNavigate } from "react-router-dom"
+import styled from "styled-components"
+import editIcon from "../assets/edit.png"
 
 const Profile = () => {
     const [editMode, setEditMode] = useState(false)
@@ -41,66 +43,186 @@ const Profile = () => {
         getDetails()
     }, [])
 
-    if (loading) return <div>Loading...</div>
+    if (loading) return <Wrapper>Loading...</Wrapper>
 
     return (
-        <div>
-            <div>
-                <p>username:</p>
-                <p>{details.username}</p>
+        <Wrapper>
+            <h2>My Details</h2>
+            <div className="content">
+                <div className="row">
+                    <p className="label">Username:</p>
+                    <p className="value">{details.username}</p>
+                </div>
+                <div>
+                    {
+                        editMode ?
+                        <form onSubmit={handleSubmit} className="details-section">
+                            <div>
+                                <p className="label">Casual Name:</p>
+                                <input
+                                    className="value" 
+                                    type="text" 
+                                    placeholder="$" 
+                                    value={newCasual} 
+                                    onChange={e => setNewCasual(e.target.value)}
+                                />
+                            </div>
+                            <div>
+                                <p className="label">Mood:</p>
+                                <select
+                                    className="value"
+                                    value={newMood}
+                                    onChange={(e) => setNewMood(e.target.value)}
+                                >
+                                    {
+                                        availableMoods.map(m => (
+                                            <option key={m} value={m}>{m}</option>
+                                        ))
+                                    }
+                                </select>
+                            </div>
+                            <div className="buttons">
+                                <button type="submit" className="save-btn">Save</button>
+                                <button 
+                                    type="button"
+                                    onClick={() => {
+                                        setNewCasual(details.casualName)
+                                        setNewMood(details.mood)
+                                        setEditMode(false)
+                                    }} 
+                                    className="cancel-btn"
+                                >Cancel</button>
+                            </div>
+                        </form>
+                        :
+                        <div className="details-section">
+                            <div>
+                                <p className="label">Casual Name:</p>
+                                {details.casualName ? <p className="value">{details.casualName}</p> : <p>_________</p>}
+                            </div>
+                            <div>
+                                <p className="label">Mood:</p>
+                                <p className="value">{details.mood}</p>
+                            </div>
+                            <button onClick={() => setEditMode(true)} className="edit-btn">Edit</button>
+                        </div>
+                    }
+                </div>
             </div>
-            <div>
-                {
-                    editMode ?
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <p>Casual Name:</p>
-                            <input 
-                                type="text" 
-                                placeholder="$" 
-                                value={newCasual} 
-                                onChange={e => setNewCasual(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <p>Mood:</p>
-                            <select
-                                value={newMood}
-                                onChange={(e) => setNewMood(e.target.value)}
-                            >
-                                {
-                                    availableMoods.map(m => (
-                                        <option key={m} value={m}>{m}</option>
-                                    ))
-                                }
-                            </select>
-                        </div>
-                        <button type="submit">Save</button>
-                        <button 
-                            type="button"
-                            onClick={() => {
-                                setNewCasual(details.casualName)
-                                setNewMood(details.mood)
-                                setEditMode(false)
-                            }} 
-                        >Cancel</button>
-                    </form>
-                    :
-                    <div>
-                        <div>
-                            <p>Casual Name:</p>
-                            {details.casualName ? <p>{details.casualName}</p> : <p>_________</p>}
-                        </div>
-                        <div>
-                            <p>Mood:</p>
-                            <p>{details.mood}</p>
-                        </div>
-                        <button onClick={() => setEditMode(true)}>Edit</button>
-                    </div>
-                }
-            </div>
-        </div>
+        </Wrapper>
     )
 }
 
 export default Profile
+
+const Wrapper = styled.div`
+    width: 50%;
+    height: 100%;
+    padding: 20px;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    border-left: 1px solid #cacaca;
+
+    @media (max-width: 1200px) {
+        width: 60%;
+    }
+
+    .content {
+        margin-top: 10rem;
+    }
+
+    h2 {
+        font-size: 1.6rem;
+        font-weight: 600;
+        margin-bottom: 20px;
+        position: absolute;
+        top: 40px;
+        left: 40px;
+        border-bottom: 1px solid #cacaca;
+    }
+
+    .row {
+        display: flex;
+        gap: 10px;
+        margin-bottom: 12px;
+    }
+
+    .label {
+        font-size: 16px;
+        font-weight: 400;
+        opacity: 0.7;
+    }
+
+    .value {
+        font-size: 16px;
+        font-weight: 500;
+    }
+
+    .details-section {
+        background-color: #eee;
+        padding: 16px;
+        position: relative;
+        width: 20rem;
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        border-radius: 10px;
+        border: 2px solid #cacaca;
+    }
+
+    input,
+    select {
+        width: 100%;
+        padding: 6px 8px;
+        margin-top: 4px;
+        border-radius: 10px;
+        border: 2px solid #cacaca;
+    }
+
+    .edit-btn {
+        position: absolute;
+        bottom: 12px;
+        right: 12px;
+        padding: 6px 10px 6px 30px;
+        background-color: transparent;
+        border: 1px solid #999;
+        cursor: pointer;
+
+        background-image: url(${editIcon});
+        background-repeat: no-repeat;
+        background-size: 14px 14px;
+        background-position: 8px center;
+    }
+
+    .save-btn {
+        background-color: #9be59b;
+        color: #000;
+        border: none;
+        padding: 6px 12px;
+        margin-right: 10px;
+        cursor: pointer;
+        border-radius: 6px;
+    }
+
+    .cancel-btn {
+        background-color: #e57373;
+        color: #fff;
+        border: none;
+        padding: 6px 12px;
+        cursor: pointer;
+        border-radius: 6px;
+    }
+
+    .buttons {
+        display: flex;
+        width: 100%;
+        justify-content: flex-end;
+    }
+
+    p {
+        margin: 0;
+    }
+`
